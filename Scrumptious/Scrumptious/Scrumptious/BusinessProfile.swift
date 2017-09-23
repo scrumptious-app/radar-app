30a763ed9390465da216467a563cabf1ebea8f87
Brian Nunes De Souza <brian.nunes.souza2@hotmail.com>
2017-09-23 13:47:27 -0400

    Creation of Business Profile

--- Scrumptious/Scrumptious/Scrumptious/BusinessProfile.swift
+++ Scrumptious/Scrumptious/Scrumptious/BusinessProfile.swift
@@ -7,12 +7,39 @@
 //
 
 import Foundation
+import UIKit
 
 class BusinessProfile{
     
-    var price: Double
+    var name: String?
+    var price: Double?
+    var rating: Double?
+    var catTitle: String?
+    var id: String?
+    var friendsImages: [UIImage]?
+    var menu: [String]?
     
-    init(){
-        self.price = 0
+    init(name: String?, price: Double?, rating: Double?, catTitle: String?, id: String?, friendsImages: [UIImage]?, menu: [String]?){
+        if name != nil{
+            self.name = name
+        }
+        if rating != nil{
+            self.rating = rating
+        }
+        if catTitle != nil{
+            self.catTitle = catTitle
+        }
+        if id != nil{
+            self.id = id
+        }
+        if price != nil{
+            self.price = price
+        }
+        if friendsImages != nil{
+            self.friendsImages = friendsImages
+        }
+        if menu != nil{
+            self.menu = menu
+        }
     }
 }
